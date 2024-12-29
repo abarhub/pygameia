1978731b3188bc60a81b45458d0379d043deb999
abarhub <abarhub@gmail.com>
2024-12-29 14:54:02 +0100

    amélioration du jeux

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # This is a sample Python script.
 import random
+import time
+from copy import deepcopy
 
-from tictactoe_game import TicTacToeGame
+from tictactoe_game import TicTacToeGame, Games, Game, Coup
 
 
 # Press Maj+F10 to execute it or replace it with your code.
@@ -10,36 +12,103 @@ from tictactoe_game import TicTacToeGame
 
 def jeux():
     jeux = TicTacToeGame()
-    jeux.joue(1,0,0)
+    jeux.joue(1, 0, 0)
     jeux.afficher()
 
 
 def jeux2():
     jeux = TicTacToeGame()
-    joueur=1
+    joueur = 1
 
-    while(True):
+    while (True):
         liste_coups = jeux.cases_possibles()
-        n=random.randint(0,len(liste_coups)-1)
-        pos=liste_coups[n]
-        jeux.joue(joueur,pos[0],pos[1])
+        n = random.randint(0, len(liste_coups) - 1)
+        pos = liste_coups[n]
+        jeux.joue(joueur, pos[0], pos[1])
         jeux.afficher()
 
         if jeux.finJeux():
             print("fin du jeux")
-            gagnant=jeux.gagnant()
-            if gagnant==1:
+            gagnant = jeux.gagnant()
+            if gagnant == 1:
                 print("gagnant: joueur 1")
-            elif gagnant==2:
+            elif gagnant == 2:
                 print("gagnant: joueur 2")
             else:
                 print("aucun gagnant")
             break
 
-        if joueur==1:
+        if joueur == 1:
             joueur = 2
         else:
             joueur = 1
+
+
+def coup_suivant(joueur, jeux, listeCoups, games):
+    liste_coups = jeux.cases_possibles()
+
+    for coup in liste_coups:
+        jeux2 = TicTacToeGame(jeux.clone_plateau())
+        listeCoups2 = deepcopy(listeCoups)
+        listeCoups2.append(Coup(coup[0], coup[1], jeux2.clone_plateau()))
+        jeux2.joue(joueur, coup[0], coup[1])
+        fin = False
+        if jeux2.finJeux():
+            #print("fin du jeux")
+            gagnant = jeux2.gagnant()
+            # if gagnant == 1:
+            #     print(f"gagnant: joueur 1 : {listeCoups2}")
+            # elif gagnant == 2:
+            #     print(f"gagnant: joueur 2 : {listeCoups2}")
+            # else:
+            #     print(f"aucun gagnant : {listeCoups2}")
+            fin = True
+            game = Game(gagnant, listeCoups2, jeux2.clone_plateau())
+            games.games.append(game)
+        else:
+            if joueur == 1:
+                joueur_suivant = 2
+            else:
+                joueur_suivant = 1
+            coup_suivant(joueur_suivant, jeux2, listeCoups2, games)
+
+    pass
+
+
+def jeux3():
+    jeux = TicTacToeGame()
+    joueur = 1
+    games = Games()
+
+    start = time.time()
+    coup_suivant(joueur, jeux, [], games)
+
+    print("duree:", time.ctime(time.time() - start)[11:19], " sec")
+
+    print("nb de jeux: ", len(games.games))
+
+    # while True:
+    #     liste_coups = jeux.cases_possibles()
+    #     n = random.randint(0, len(liste_coups) - 1)
+    #     pos = liste_coups[n]
+    #     jeux.joue(joueur, pos[0], pos[1])
+    #     jeux.afficher()
+    #
+    #     if jeux.finJeux():
+    #         print("fin du jeux")
+    #         gagnant = jeux.gagnant()
+    #         if gagnant == 1:
+    #             print("gagnant: joueur 1")
+    #         elif gagnant == 2:
+    #             print("gagnant: joueur 2")
+    #         else:
+    #             print("aucun gagnant")
+    #         break
+    #
+    #     if joueur == 1:
+    #         joueur = 2
+    #     else:
+    #         joueur = 1
 
 
 def print_hi(name):
@@ -52,5 +121,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     jeux()
     jeux2()
+    jeux3()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- tictactoe_game.py
+++ tictactoe_game.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 
 JOUEUR1 = -1
 JOUEUR2 = 1
@@ -5,8 +6,11 @@ JOUEUR2 = 1
 
 class TicTacToeGame:
 
-    def __init__(self):
-        self.plateau = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    def __init__(self, plateau=None):
+        if plateau is None:
+            self.plateau = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        else:
+            self.plateau = deepcopy(plateau)
 
     def afficher(self):
         s = ""
@@ -24,34 +28,34 @@ class TicTacToeGame:
         print("jeux:")
         print(s)
 
-    def valeurCase(self,x,y):
-        if x<0 or x>2 or y<0 or y>2:
+    def valeurCase(self, x, y):
+        if x < 0 or x > 2 or y < 0 or y > 2:
             raise Exception(f"la position n'est pas valide: x={x}, y={y}")
-        n=self.plateau[x][y]
+        n = self.plateau[x][y]
         return self.valeurCase2(n)
 
-    def valeurCase2(self,n):
-        if n==0:
+    def valeurCase2(self, n):
+        if n == 0:
             return " "
-        elif n==JOUEUR1:
+        elif n == JOUEUR1:
             return "X"
-        elif n==JOUEUR2:
+        elif n == JOUEUR2:
             return "O"
         else:
             raise Exception(f"la valeur à la position n'est pas valide: {n}")
 
-    def joue(self,noJoueur,x,y):
-        if x<0 or x>2 or y<0 or y>2:
+    def joue(self, noJoueur, x, y):
+        if x < 0 or x > 2 or y < 0 or y > 2:
             raise Exception(f"la position n'est pas valide: x={x}, y={y}")
-        if noJoueur<1 or noJoueur>2:
+        if noJoueur < 1 or noJoueur > 2:
             raise Exception(f"le joueur doit avoir la valeur entre 1 et 2 (joueur={noJoueur})")
-        if self.plateau[x][y] !=0:
-            n=self.valeurCase(x,y)
+        if self.plateau[x][y] != 0:
+            n = self.valeurCase(x, y)
             raise Exception(f"la position ({x},{y}) a deja une valeur: {n}")
         if noJoueur == 1:
-            self.plateau[x][y]=-1
+            self.plateau[x][y] = -1
         elif noJoueur == 2:
-            self.plateau[x][y]=1
+            self.plateau[x][y] = 1
 
     def finJeux(self):
 
@@ -63,26 +67,26 @@ class TicTacToeGame:
         #         if len(valeur) == 1:
         #             return True
 
-        valeurLignes=dict()
+        valeurLignes = dict()
         valeurColonnes = dict()
         diag1 = set()
         diag2 = set()
-        caseIncomplete=False
+        caseIncomplete = False
         for i in range(3):
             for j in range(3):
-                val=self.plateau[i][j]
+                val = self.plateau[i][j]
                 if i not in valeurLignes:
                     valeurLignes[i] = set()
                 valeurLignes[i].add(val)
                 if j not in valeurColonnes:
                     valeurColonnes[j] = set()
                 valeurColonnes[j].add(val)
-                if i==j:
+                if i == j:
                     diag1.add(val)
-                if i+j==2:
+                if i + j == 2:
                     diag2.add(val)
-                if val==0:
-                    caseIncomplete=True
+                if val == 0:
+                    caseIncomplete = True
 
         for valeur in valeurLignes:
             if len(valeurLignes[valeur]) == 1 and 0 not in valeurLignes[valeur]:
@@ -92,9 +96,9 @@ class TicTacToeGame:
             if len(valeurColonnes[valeur]) == 1 and 0 not in valeurColonnes[valeur]:
                 return True
 
-        if len(diag1)==1 and 0 not in diag1:
+        if len(diag1) == 1 and 0 not in diag1:
             return True
-        if len(diag2)==1 and 0 not in diag2:
+        if len(diag2) == 1 and 0 not in diag2:
             return True
 
         if caseIncomplete:
@@ -104,12 +108,12 @@ class TicTacToeGame:
 
     def cases_possibles(self):
 
-        liste=[]
+        liste = []
 
         for i in range(3):
             for j in range(3):
                 val = self.plateau[i][j]
-                if val==0:
+                if val == 0:
                     liste.append((i, j))
 
         return liste
@@ -124,26 +128,26 @@ class TicTacToeGame:
         #         if len(valeur) == 1:
         #             return True
 
-        valeurLignes=dict()
+        valeurLignes = dict()
         valeurColonnes = dict()
         diag1 = set()
         diag2 = set()
-        caseIncomplete=False
+        caseIncomplete = False
         for i in range(3):
             for j in range(3):
-                val=self.plateau[i][j]
+                val = self.plateau[i][j]
                 if i not in valeurLignes:
                     valeurLignes[i] = set()
                 valeurLignes[i].add(val)
                 if j not in valeurColonnes:
                     valeurColonnes[j] = set()
                 valeurColonnes[j].add(val)
-                if i==j:
+                if i == j:
                     diag1.add(val)
-                if i+j==2:
+                if i + j == 2:
                     diag2.add(val)
-                if val==0:
-                    caseIncomplete=True
+                if val == 0:
+                    caseIncomplete = True
 
         for valeur in valeurLignes:
             if len(valeurLignes[valeur]) == 1 and 0 not in valeurLignes[valeur]:
@@ -163,14 +167,14 @@ class TicTacToeGame:
                 else:
                     return 0
 
-        if len(diag1)==1 and 0 not in diag1:
+        if len(diag1) == 1 and 0 not in diag1:
             if JOUEUR1 in diag1:
                 return 1
             elif JOUEUR2 in diag1:
                 return 2
             else:
                 return 0
-        if len(diag2)==1 and 0 not in diag2:
+        if len(diag2) == 1 and 0 not in diag2:
             if JOUEUR1 in diag2:
                 return 1
             elif JOUEUR2 in diag2:
@@ -180,3 +184,31 @@ class TicTacToeGame:
 
         return 0
 
+    def clone_plateau(self):
+        return deepcopy(self.plateau)
+
+
+class Coup:
+    def __init__(self, x, y, plateau):
+        self.x = x
+        self.y = y
+        self.plateau = plateau
+
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Game:
+    def __init__(self, noJoueurGagnant, listeCoups, plateau):
+        self.noJoueurGagnant = noJoueurGagnant
+        self.listeCoups = listeCoups
+        self.plateau = plateau
+
+
+class Games():
+
+    def __init__(self):
+        self.games = []
